be9365b82b5b41fe4b01039761da02d88cfc4eb6
Ankur Gupta <ankurdotcom@gmail.com>
2023-10-02 20:53:19 +0530

    Chapter-4 : conditional statement

---
+++ 04_conditional/conditional.js
@@ -0,0 +1,32 @@
+
+// if Else
+const studentAge = 36;
+
+const validVotingAge = 18;
+
+if (studentAge >= validVotingAge) {
+    console.log('Congratulation !!! You can Vote !!!');
+} else {
+    console.log('Sorry !!! You can not Vote !!!');
+}
+
+
+// Ternary operator
+
+studentAge >= validVotingAge ? console.log('Congratulation !!! You can Vote ') :console.log('Sorry !!! You can not Vote ');
+
+//Switch Statement
+
+inputkey = 0
+
+switch(inputkey){
+    case 1 : console.log(` Case 1`);
+    break;
+    case 2 : console.log(` Case 2`);
+    break;
+    case 3 : console.log(` Case 3`);
+    break;
+    default : console.log(` Invalid Input`);
+
+}
+
